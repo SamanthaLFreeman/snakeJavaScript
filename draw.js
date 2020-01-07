bbe3880caa698e60afc5714d8b328f37eb3af82e
Alan Turing <freemasl.sf@gmail.com>
2020-01-07 09:18:17 -0700

    Create rows and columns in the canvas

--- draw.js
+++ draw.js
@@ -1,2 +1,6 @@
 const canvas = document.querySelector(".canvas");
-console.log(canvas)
+const contextCanvas = canvas.getContext("2d");
+const scale = 10;
+
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
