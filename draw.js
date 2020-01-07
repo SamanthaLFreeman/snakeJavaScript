34cf430a450770756782b0c66ca155c0de4521e8
Alan Turing <freemasl.sf@gmail.com>
2020-01-07 09:27:40 -0700

    Create moving animation for the snake, but doesn't follow key presses

--- draw.js
+++ draw.js
@@ -6,3 +6,13 @@ const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
 var snake;
+
+(function setup() {
+  snake = new Snake();
+
+  window.setInterval(() => {
+    contextCanvas.clearRect(0, 0, canvas.width, canvas.height);
+    snake.update();
+    snake.draw();
+  }, 250)
+}());
